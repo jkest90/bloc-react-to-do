29ab43d1cbf00e13820db3a386f3333130f33fef
jkest90 <jkest618@gmail.com>
2018-03-12 20:03:13 -0600

    complete:checkpoint 2-4

---
+++ src/components/ToDo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+    render() {
+        return (
+            <li> A todo will go here </li>
+        );
+    }
+}
+
+
+// Once a component is exported, it can be imported anywhere we want to use it.
+export default ToDo;
