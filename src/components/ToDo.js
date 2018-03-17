4ccbd1c3de33ab0fa6a0280d0d2e53b69c4c5db5
jkest90 <jkest618@gmail.com>
2018-03-17 17:37:28 -0600

    eslint attempt

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 // A child of the 'App' component. Data for this.props is passed down from it in App.js.
 class ToDo extends Component {
-   render() {
-      return (
-         <li>
-            <input type="checkbox" checked={ this.props.isCompleted } />
-            <span> { this.props.description } </span>
-         </li>
-      );
-    }
+  render() {
+    return (
+      <li>
+        <input type="checkbox" checked={this.props.isCompleted} />
+        <span> { this.props.description } </span>
+      </li>
+    );
+  }
 }
 
 // Once a component is exported, it can be imported anywhere we want to use it.
