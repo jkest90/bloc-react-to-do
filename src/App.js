55d5e0bf99e668a53a832bd6474d969f6ade9000
jkest90 <jkest618@gmail.com>
2018-03-16 21:01:44 -0600

    checkpoint 2.6: complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <ul>
-              <ToDo />
-              <ToDo />
-          </ul>
-      </div>
-    );
-  }
+   constructor(props) {
+      super(props);
+      this.state = {
+         todos: [
+            { description: 'Walk the cat', isCompleted: true },
+            { description: 'Throw the dishes away', isCompleted: false },
+            { description: 'Buy new dishes', isCompleted: false }
+         ]
+      };
+   }
+   render() {
+      return (
+        <div className="App">
+            <ul>
+               { this.state.todos.map( (todo, index) =>
+                  <ToDo key= { index } description={ todo.description } isCompleted={ todo.isCompleted } />
+               )}
+            </ul>
+         </div>
+      );
+    }
 }
 
 export default App;
+
+// Checkpoint 2.6 //
+/*
+Step 1: Define constructor function that calls super (its parent's constructor) and own constructor.
+Step 2: Define this.state as an array of to-do objects with properties of 'description' and 'isCompleted'.
+Step 3: Display the to-dos within this.state using .map(), returning a new array of each item. Enclose the call to map with { }, which tells JSX to evaluate the code between braces as JS. For React to operate properly, each child of an array or iterator needs to have a key with a unique value. Specify a 'key' attribute on ToDo equal to each item's index. This gives us a unique identifier for each item.
+*/
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
+// A child of the 'App' component. Data for this.props is passed down from it in App.js.
 class ToDo extends Component {
-    render() {
-        return (
-            <li> A todo will go here </li>
-        );
+   render() {
+      return (
+         <li>
+            <input type="checkbox" checked={ this.props.isCompleted } />
+            <span> { this.props.description } </span>
+         </li>
+      );
     }
 }
-
 
 // Once a component is exported, it can be imported anywhere we want to use it.
 export default ToDo;
